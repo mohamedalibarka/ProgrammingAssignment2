73a288eb8323df3ef05ffeb812d0beef6584547c
MohamedAli-Fantasiapp <70633872+MohamedAli-Fantasiapp@users.noreply.github.com>
2020-11-28 22:47:48 +0100

    makeCacheMatrix and cacheSolve  fuctions to calculate and store the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This funtion creates a vector containing 4 fuctions used to store  the input 
+## matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv<-NULL
+    set<- function(mat) {
+        x<<- mat
+        inv<<-NULL
+    }
+    get<- function() x
+    setInv<- function(inver) inv<<-inver
+    getInv<- function() inv
+    list(set=set,get=get,setInv=setInv,getInv=getInv)
 }
 
 
-## Write a short comment describing this function
+## Checks if the inverse matrix has already been calculated; if not it
+## calculates it then stores it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv<-x$getInv()
+        if (!is.null(inv)) {
+            message("Getting cashed inversed matrix")
+            return(inv)
+        }
+        matrix<-x$get()
+        invMatrix<-solve(matrix)
+        x$setInv(invMatrix)
+        invMatrix
 }
